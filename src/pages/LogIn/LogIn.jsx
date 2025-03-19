ac0e31a5a128dde8952e89906622f35ca4359ba7
Mohammed Sameh <mohamed.sameh.abdelzaher@gmail.com>
2025-03-19 20:09:26 +0200

    submission of login and sign up

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
-// import { useState } from 'react'
-import '../../assets/css/LogIn/LogIn.css'
+import React, { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import '../../assets/css/LogIn/LogIn.css';
+import LogInLogo from "./LogInLogo/LogInLogo";
+import LogInFooter from "./LogInFooter/LogInFooter";
+import ToolTip from "./ToolTip/ToolTip";
 
-function LogIn() {
+const LogIn = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [step, setStep] = useState(1);
+    const [showHelp, setShowHelp] = useState(false);
 
-}
+    const handleNext = () => {
+      if (email) setStep(2);
+    };
 
-export default LogIn
+    const handleLogin = () => {
+      if (password) alert("Login Successful!");
+    };
+
+    return (
+        <>
+            <LogInLogo/>
+            <div className="login-container">
+                <div className="login-box">
+                    <div className="login-box-inside">
+                        <div className="sign_in_txt">
+                            <p>Sign in</p>
+                        </div>
+                        {step === 1 ? (
+                            <>
+                                <div className="email_input">
+                                    <label class="label-input">Email or mobile phone number</label>
+                                </div>
+                                <input
+                                    type="text"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                                <button onClick={handleNext} className="btn-primary continue">Continue</button>
+                                <div className="links-down">
+                                    <p className="terms-and-condition"> By continuing, you agree to the Amazon <a href="https://www.primevideo.com/ww-av-legal-home/ref=av_auth_te">Conditions of Use and Privacy Notice</a>.</p>
+                                    {/* <a href="#" className="forgot-password">Forgot your password?</a> */}
+                                </div>
+                                <div className="help-container">
+
+                                <button className="help-button" onClick={() => setShowHelp(!showHelp)}>
+                                    <i
+                                    className={`fa-solid fa-angle-down ${showHelp ? "rotate" : ""}`}
+                                    ></i>
+                                    Need help?
+                                </button>
+
+                                {showHelp && <p className="forgot-password">Forgot your password?</p>}
+                                </div>
+                            </>
+                        ) : (
+                            <>
+                                <div className="email-change">
+                                    <p>{email}</p>
+                                    {/* <Link to="/LogIn" className="home-link-change">Change</Link> */}
+                                    <Link to="#" className="home-link-change" onClick={() => setStep(1)}>Change</Link>
+                                </div>
+                                {/* <div className="password_input_and_forgot">
+                                    <label class="label-input">Password</label>
+                                    <a href="#" >Forgot password?</a>
+                                </div> */}
+
+
+                                <input
+                                    type="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                                <button onClick={handleLogin} className="btn-primary sign-in">Sign in</button>
+                                <ToolTip/>
+                            </>
+                        )}
+                    </div>
+                </div>
+                <div className="new-to-amazon">
+                    <div className="new-to-amazon-line">
+                        <div className="new-to-amazon-txt">New to Amazon?</div>
+                        <hr />
+                    </div>
+                    <Link to="/signup" className="sign-up-btn">
+                        Create your Amazon account
+                    </Link>
+                </div>
+                <LogInFooter/>
+            </div>
+        </>
+    );
+};
+
+export default LogIn;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
-// import { useState } from 'react'
-import '../../assets/css/SignUp/SignUp.css'
+import { useState, React } from 'react';
+import { Link } from "react-router-dom";
+import '../../assets/css/SignUp/SignUp.css';
+import LogInLogo from "../LogIn/LogInLogo/LogInLogo";
+import LogInFooter from '../LogIn/LogInFooter/LogInFooter';
 
 function SignUp() {
+    const [useEmail, setUseEmail] = useState(false);
+    return (
+        <>
+            <LogInLogo/>
+            <div className="signup-container">
+                <div className="signup-box">
+                    <form action="{{url_for(SignUp)}" method="POST">
+                        <h2>Create Account</h2>
+                        <p className="required-text">All fields are required</p>
+                        
+                        <label className="input-label">Your name</label>
+                        <input type="text" placeholder="First and last name" className="input-field" />
+                    
+                        <label className="input-label">
+                            {useEmail ? "Email" : "Mobile number"}
+                        </label>
 
-}
+                        <div className="mobile-input">
+                            {!useEmail ? (
+                            <>
+                                <select className="country-code">
+                                <option>EG +20</option>
+                                <option>GB +44</option>
+                                <option>US +1</option>
+                                <option>IN +91</option>
+                                </select>
+                                <input type="text" placeholder="Mobile number" className="input-field" />
+                            </>
+                            ) : (
+                            // إدخال البريد الإلكتروني
+                            <input type="email" placeholder="Enter your email" className="input-field" />
+                            )}
+                        </div>
 
-export default SignUp
+                        <a
+                            href="#"
+                            className="email-link"
+                            onClick={(e) => {
+                            e.preventDefault();
+                            setUseEmail(!useEmail); 
+                            }}
+                        >
+                            {useEmail ? "Use your phone number instead" : "Use your email instead"} 
+                            <i className="fa-solid fa-angle-right"></i>
+                        </a>
+
+                        <label className="input-label">Password</label>
+                        <input type="password" placeholder="At least 6 characters" className="input-field" />
+                        <p className="password-info"><i class="fa-solid fa-circle-info"></i>Passwords must be at least 6 characters.</p>
+                        
+                        <p className="verification-info">To verify your number, we will send you a text message with a temporary code. Message and data rates may apply.</p>
+                        
+                        <button className="continue-btn" type="submit">Create Account</button>
+
+                    </form>
+
+                    <div className="break_line"></div>
+                    
+                    <p className="signin-link">Already have an account? <Link to="/login">Sign in <i class="fa-solid fa-angle-right"></i></Link></p>
+                </div>
+            </div>
+            <LogInFooter/>
+        </>
+    );
+};
+
+export default SignUp;
+
