7877a8c5fa64f5886b12f62456ba835aaf9c4859
Moamen-Youssef <113758180+Moamen-Youssef@users.noreply.github.com>
2025-03-20 14:17:51 +0200

    "testing applying changes to the repo"

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Link, Outlet } from "react-router-dom";
-import './assets/fontawesome-free-6.6.0-web/css/all.css'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+import "./assets/fontawesome-free-6.6.0-web/css/all.css";
 function App() {
-
+  let c = 0; // this is for testing applying changes to the repo, if you see it you could delete it :)
   return (
     <>
       <h1>Home page</h1>
-      <Link to="/LogIn">Log In</Link> 
+      <Link to="/LogIn">Log In</Link>
       <Link to="/SignUp">Sign Up</Link>
       <Outlet />
     </>
   );
-};
+}
 
 export default App;
