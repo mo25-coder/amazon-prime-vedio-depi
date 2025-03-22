fdb7a4b42d7686919ead7b4f67fe5f06f3a24b98
m7mdbic <16971248+m7mdbic@users.noreply.github.com>
2025-03-22 02:26:07 +0200

    Update Footer.jsx
    footer component jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-// import { useState } from 'react'
-import '../../assets/css/Footer/Footer.css'
+import { SiContentful, SiPrimevideo } from "react-icons/si";
+import "./Footer.css";
+import { FaFileExcel } from "react-icons/fa";
 
-function Footer() {
+export default function Footer() {
+  //   const [hidden, setHidden] = useState(true);
+  //   const [userHidden, setUserHidden] = useState(true);
+  //   const [menuHidden, setMenuHidden] = useState(true);
 
+  return (
+    <div className="footerContainer">
+      <div className="footerLogo">
+        <SiPrimevideo className="footerLogoImg" />
+        {/* <img src="../src/assets/prime.svg" alt="Prime Video Logo" className="footerLogoImg" /> */}
+      </div>
+
+      <div className="footerLstContainer">
+        <ul className="footerLst">
+          <li className="footerLstItem">
+            <a href="/" className="footerLstLink">
+              Terms and Privacy Notice
+            </a>
+          </li>
+          <li className="footerLstItem">
+            <a href="/" className="footerLstLink">
+              Send us feedback
+            </a>
+          </li>
+          <li className="footerLstItem">
+            <a href="/" className="footerLstLink">
+              Help
+            </a>
+          </li>
+          <li className="footerLstItem">
+            <address className="footerLstAdd">
+              © 2025, amazon prime clone project, or its affiliates
+            </address>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
-
-export default Footer
