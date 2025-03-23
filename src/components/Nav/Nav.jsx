45ea55f9cd9815c873a852f3af98ac29791333b9
m7mdbic <16971248+m7mdbic@users.noreply.github.com>
2025-03-23 02:03:57 +0200

    Update Nav.jsx
    updating navbar component , adding my stuff icon and drop menu

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import "..src/assets/css/Nav/Nav.css";
-import { FaSearch, FaUserCircle } from "react-icons/fa";
+import { FaSearch, FaUserCircle, FaBookmark } from "react-icons/fa";
 import { CgMenuGridO } from "react-icons/cg";
 import { SiPrimevideo } from "react-icons/si";
 
@@ -9,9 +9,12 @@ import { useState } from "react";
 
 export default function Navbar() {
   //   const [hidden, setHidden] = useState(true);
-  const [userHidden, setUserHidden] = useState(true);
+  //   const [userHidden, setUserHidden] = useState(true);
+  const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
+
   //   const [menuHidden, setMenuHidden] = useState(true);
   const [menuOpen, setMenuOpen] = useState(false);
+  const [myStuffOpen, setMyStuffOpen] = useState(false);
 
   //   let menuRef = useRef();
 
@@ -59,21 +62,19 @@ export default function Navbar() {
 
       {/* Search and User Section */}
       <div className="navbarRightContainer">
-        <div className="navSearchContainer">
-          <div className="navSearch">
-            {/* <input type="text" placeholder="Search" className="searchInput" /> */}
-            <FaSearch
-              className="searchIcon"
-              //   onClick={(hidden) => setHidden(!hidden)}
-            />
-            {/* </div> */}
-          </div>
+        {/* <div className="navSearchContainer"> */}
+        <div className="navSearch">
+          {/* <input type="text" placeholder="Search" className="searchInput" /> */}
+          <FaSearch
+            className="searchIcon"
+            //   onClick={(hidden) => setHidden(!hidden)}
+          />
+          {/* </div> */}
         </div>
-
         <div className="navbarMenuContainer">
           {/* <a href="/"> */}
           <CgMenuGridO
-            className="navbarHamIcon"
+            className="navbarMenuIcon"
             //   onMouseEnter={() => setMenuHidden(!menuHidden)}
             //   onMouseLeave={() => setMenuHidden(!menuHidden)}
             onClick={() => setMenuOpen(!menuOpen)}
@@ -98,70 +99,40 @@ export default function Navbar() {
               <DropdownItem text={"Thrillers"} />
               <DropdownItem text={"Romance"} />
               <DropdownItem text={"SciFi"} />
-              {/* <li>
-                  <a href="/" className="navbar__link">
-                    <span>Action and Adventure</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="/" className="navbar__link">
-                    <span>Comedy</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="/" className="navbar__link">
-                    <span>Documentary</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="/" className="navbar__link">
-                    <span>Drama</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="/" className="navbar__link">
-                    <span>Fantasy</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="/" className="navbar__link">
-                    <span>Horror</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="/" className="navbar__link">
-                    <span>Kids</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="/" className="navbar__link">
-                    <span>Mystery and Thrillers</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="/" className="navbar__link">
-                    <span>Romance</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="/" className="navbar__link">
-                    <span>SciFi</span>
-                  </a>
-                </li> */}
             </ul>
           </div>
           {/* )} */}
         </div>
+        <div
+          className="navbarMyStuff"
+          onClick={() => setMyStuffOpen(!myStuffOpen)}
+          //   onMouseEnter={() => setIsMenuOpen(true)}
+          //   onMouseLeave={() => setIsMenuOpen(false)}
+        >
+          <FaBookmark className="myStuffIcon" />
 
-        <div className="navbarUserContainer">
+          {myStuffOpen && (
+            <div className="myStuffMenu">
+              <div>My Stuff</div>
+              <ul className="myStuffMenuLst">
+                <DropdownItem text={"All"} />
+              </ul>
+            </div>
+          )}
+        </div>
+        <div
+          className="navbarUserContainer"
+          onMouseEnter={() => setIsUserMenuOpen(true)}
+          onMouseLeave={() => setIsUserMenuOpen(false)}
+        >
           <FaUserCircle
             className="navbarUserIcon"
-            onMouseEnter={() => setUserHidden(false)}
-            onMouseLeave={() => setUserHidden(true)}
+            // onMouseEnter={() => setUserHidden(false)}
+            // onMouseLeave={() => setUserHidden(true)}
           />
 
           {/* User Dropdown Menu */}
-          {userHidden ? null : (
+          {/* {userHidden ? null : (
             <div className="userMenu">
               <ul className="userMenuLst">
                 <li className="userMenuLstItem">
@@ -174,6 +145,14 @@ export default function Navbar() {
                     <span>Sign Out</span>
                   </a>
                 </li>
+              </ul>
+            </div>
+          )} */}
+          {isUserMenuOpen && (
+            <div className="userMenu">
+              <ul className="userMenuLst">
+                <DropdownItem text={"Account"} />
+                <DropdownItem text={"Sign Out"} />
               </ul>
             </div>
           )}
