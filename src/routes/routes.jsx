733c87b497c96a7ef9b8e79e99bad87b0f3b77f6
Mohammed Sameh <mohamed.sameh.abdelzaher@gmail.com>
2025-04-10 22:02:20 +0200

    Update routes.jsx

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -9,7 +9,8 @@ import ManageAccounts from "../pages/ManageAccounts/ManageAccounts";
 import MyStuff from "../pages/MyStuff/MyStuff";
 import ShowDetails from "../pages/ShowDetails/ShowDetails";
 import Category from "../pages/Categories/Category";
-
+// new
+import GuestPage from "../pages/GuestPage/GuestPage";
 import "../assets/fontawesome-free-6.6.0-web/css/all.css";
 import Media from "../pages/Media/Media";
 
@@ -57,6 +58,10 @@ const router = createBrowserRouter([
       {
         path: "ShowDetails",
         element: <ShowDetails />,
+      },
+      {
+        path: "GuestPage",
+        element: <GuestPage />,
       },
       {
         path: "",
