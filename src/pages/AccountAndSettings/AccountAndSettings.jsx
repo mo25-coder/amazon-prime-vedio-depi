804bba9c6dc792b697a83dd8f5dfb4bc196c52d6
Mohamed Sameh <mohamed.sameh.abdelzaher@gmail.com>
2025-04-11 01:25:36 +0200

    Update AccountAndSettings.jsx

--- src/pages/AccountAndSettings/AccountAndSettings.jsx
+++ src/pages/AccountAndSettings/AccountAndSettings.jsx
@@ -19,64 +19,66 @@ function AccountAndSettings() {
           </div>
     
           <hr />
-    
-          <div className="settings-wc1">
-            <div className="settings-wc1-1">
-              <h4>Your details</h4>
+          <div className="settings-content">
+            <div className="settings-wc1 txt-blk">
+              <div className="settings-wc1-1 txt-blk">
+                <h4>Your details</h4>
+              </div>
+              <div className="settings-wc1-2 settings-btn">
+                <p className='bold-lighter'>Change your Name, Email, Mobile Number, Password, and more.</p>
+                <button>
+                  <a href="https://www.amazon.com/ap/cnep/ref=atv_set_ya_cha_acc_det?...">
+                    <i className="fa-solid fa-arrow-up-right-from-square">
+                      <span>Edit on Amazon</span>
+                    </i>
+                  </a>
+                </button>
+              </div>
             </div>
-            <div className="settings-wc1-2 settings-btn">
-              <p>Change your Name, Email, Mobile Number, Password, and more.</p>
-              <button>
-                <a href="https://www.amazon.com/ap/cnep/ref=atv_set_ya_cha_acc_det?...">
-                  <i className="fa-solid fa-arrow-up-right-from-square">
-                    <span>Edit on Amazon</span>
-                  </i>
-                </a>
-              </button>
-            </div>
-          </div>
-    
-          <hr />
-    
-          <div className="settings-wc2">
-            <div className="settings-wc2-1">
-              <h4>Prime membership</h4>
-            </div>
-            <br />
-            <div className="settings-wc2-2 settings-btn">
-              <p>
-                Please go to the Amazon website to view details or make changes to your Prime membership.
-              </p>
-              <button>
-                <a href="https://amazon.eg/gp/primecentral/ref=atv_set_ya_mem_edt_on_amz">
-                  <i className="fa-solid fa-arrow-up-right-from-square">
-                    <span>Manage or Cancel</span>
-                  </i>
-                </a>
-              </button>
-            </div>
-    
-            <br />
-    
-            <div className="settings-wc2-21">
-              <p>Payment method</p>
+      
+            <hr />
+      
+            <div className="settings-wc2 txt-blk">
+              <div className="settings-wc2-1">
+                <h4>Prime membership</h4>
+              </div>
               <br />
-            </div>
-    
-            <div className="settings-wc2-2-2 settings-btn">
-              <p>
-                Changing this payment method also updates your payment method for Prime membership and add-on
-                subscriptions on Prime videos.
-              </p>
-              <button>
-                <a href="https://amazon.eg/gp/primecentral/ref=atv_set_ya_mem_edt_on_amz">
-                  <i className="fa-solid fa-arrow-up-right-from-square">
-                    <span>Manage Payment method</span>
-                  </i>
-                </a>
-              </button>
+              <div className="settings-wc2-2 settings-btn">
+                <p className='bold-lighter'>
+                  Please go to the Amazon website to view details or make changes to your Prime membership.
+                </p>
+                <button>
+                  <a href="https://amazon.eg/gp/primecentral/ref=atv_set_ya_mem_edt_on_amz">
+                    <i className="fa-solid fa-arrow-up-right-from-square">
+                      <span>Manage or Cancel</span>
+                    </i>
+                  </a>
+                </button>
+              </div>
+      
+              <br />
+      
+              <div className="settings-wc2-21">
+                <p>Payment method</p>
+                <br />
+              </div>
+      
+              <div className="settings-wc2-2-2 settings-btn">
+                <p className='bold-lighter'>
+                  Changing this payment method also updates your payment method for Prime membership and add-on
+                  subscriptions on Prime videos.
+                </p>
+                <button>
+                  <a href="https://amazon.eg/gp/primecentral/ref=atv_set_ya_mem_edt_on_amz">
+                    <i className="fa-solid fa-arrow-up-right-from-square">
+                      <span>Manage Payment method</span>
+                    </i>
+                  </a>
+                </button>
+              </div>
             </div>
           </div>
+
         </div>
       );
 }
