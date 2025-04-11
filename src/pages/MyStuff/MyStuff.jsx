b2f6f0333be06975c9a3ed4ad9a9c351fb5e47da
m7mdbic <16971248+m7mdbic@users.noreply.github.com>
2025-04-12 00:32:15 +0200

    Update MyStuff.jsx

--- src/pages/MyStuff/MyStuff.jsx
+++ src/pages/MyStuff/MyStuff.jsx
@@ -1,29 +1,37 @@
 import React from "react";
-import '../../assets/css/MyStuff/MyStuff.css';
+import "../../assets/css/MyStuff/MyStuff.css";
+import Navbar from "../../components/Nav/Nav";
+import Footer from "../../components/Footer/Footer";
 
 const MyStuff = () => {
   return (
-    <div className="myStuffPage">
+    <>
+      <Navbar />
       <div>
-        <h1>My Stuff</h1>
+        <div className="myStuffPage">
+          <div className="myStuffHeader">
+            <h1>My Stuff</h1>
+          </div>
+          <div className="sub1">
+            <h2 classname="subHeader">Watch List</h2>
+            <p classname="subPara">
+              Your Wishlist is Empty, Add <a href="/">TV shows</a> and{" "}
+              <a href="/">Movies</a> that you want to watch later by clicking
+              Add to Watch list.
+            </p>
+          </div>
+          <div className="sub2">
+            <h2 classname="subHeader">Pre-orders</h2>
+            <p classname="subPara">
+              You don't have any Pre-orders, If videos you were expecting aren't
+              shown, make sure you're signed in and using the correct Amazon
+              account and then <a href="#">refresh this page</a>.
+            </p>
+          </div>
+        </div>
       </div>
-      <div>
-        <h2>Watchlist</h2>
-        <p>
-          Your Wishlist is Empty, Add <a href="#">TV shows</a> and{" "}
-          <a href="#">Movies</a> that you want to watch later by clicking Add to
-          Watch list.
-        </p>
-      </div>
-      <div>
-        <h2>Pre-orders</h2>
-        <p>
-          You don't have any Pre-orders, If videos you were expecting aren't
-          shown, make sure you're signed in and using the correct Amazon account
-          and then <a href="#">refresh this page</a>.
-        </p>
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
