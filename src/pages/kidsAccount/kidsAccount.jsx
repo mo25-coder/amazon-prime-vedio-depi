23d60bf0993ce7076ca78686cc9eace09396b748
m7mdbic <16971248+m7mdbic@users.noreply.github.com>
2025-04-12 09:03:31 +0200

    Update kidsAccount.jsx

--- src/pages/kidsAccount/kidsAccount.jsx
+++ src/pages/kidsAccount/kidsAccount.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import "../../assets/css/kidsAccount/kidsAccount.css";
+import Navbar from "../../components/Nav/Nav";
+import Footer from "../../components/Footer/Footer";
 
 function KidsAccount() {
-    return <h1>Kids Account</h1>;
+  return (
+    <>
+      <Navbar />
+
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <h1>Kids Account</h1>
+      <Footer />
+    </>
+  );
 }
 
 export default KidsAccount;
